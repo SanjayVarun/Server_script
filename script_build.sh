50e428d53278b0358a3fed606412a04c94a6d2a6
SanjayVarun <sanjay.varun95@gmail.com>
2016-12-01 10:36:38 +0530

    Added Host and User

--- script_build.sh
+++ script_build.sh
@@ -4,6 +4,9 @@
 export USE_CCACHE=1
 export CCACHE_DIR=/home/ccache/sanjay
 prebuilts/misc/linux-x86/ccache/ccache -M 50G
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export KBUILD_BUILD_USER=SanjayVarun
+export KBUILD_BUILD_HOST=SpeedHorn
 
 # clean
 make clean && make clobber
